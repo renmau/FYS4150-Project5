8cc00db476bfea8ea564ecc26de042eedd230c93
sefthus <elisabeth.strom95@gmail.com>
2017-11-30 21:01:35 +0100

    something rong in calculating the slope

--- build-Project5-Desktop_Qt_5_9_1_clang_64bit-Debug/plotting.py
+++ build-Project5-Desktop_Qt_5_9_1_clang_64bit-Debug/plotting.py
@@ -1,5 +1,8 @@
+import matplotlib as mpl
+mpl.use('TkAgg')
 import matplotlib.pyplot as plt
 import numpy as np
+from scipy.stats import linregress
 
 def plot_formatting(fam='serif',fam_font='Computer Modern Roman',font_size=18,tick_size=18):
 	""" you get to define what font and size of xlabels and axis ticks you"""
@@ -16,29 +19,40 @@ plot_formatting()
 
 #m = np.genfromtxt('d_alpha_05_lambda_0_1e5trans_1e3sim.txt')
 #m = np.genfromtxt('a_1e5trans_1e3sim.txt')
-m = np.genfromtxt('e_gamma_1_alpha_1_lambda_0_1e5trans_1e3sim.txt')
-print m
+#m = np.genfromtxt('a_gamma_0_alpha_0_lambda_0_1e5trans_1e3sim_.txt')
+m = np.genfromtxt('e_gamma_1_alpha_2_lambda_0_1e5trans_1e3sim.txt')
+
 
 #Gibbs distribution:
 beta=1./np.mean(m)
-alpha=0.5
-a=1.
+alpha=2.
+a=1.	# find a from calculating slope of simulated result
 wm=beta*np.exp(-beta*m)
-wmm=a*m**(-1-alpha)
-
+binsize=0.1
 weights = np.ones_like(m)/float(len(m))
-
 n = np.max(m)
-#counts,edges=np.histogram(m,normed=True)
-counts,edges=np.histogram(m,bins=np.arange(np.amin(m),np.amax(m)+n/10.,n/10.),normed=True)
-centers = (edges[:-1] + edges[1:])/2.0
 
+counts,edges=np.histogram(m*beta,bins=np.arange(np.amin(m*beta),np.amax(m*beta+binsize),binsize),weights=weights)
 
-#plt.hist(m,bins=np.arange(np.amin(m),np.amax(m)+n/20.,n/20.),normed=True)
-#plt.plot(m,wm)
+centers = (edges[:-1] + edges[1:])/2.
 
+#a=linregress(centers,counts)[0] #something is wrong here
+#a=linregress(counts,centers)[0]
+print 'slope =', a
+wmm=a*(m*beta)**(-1-alpha)
+plt.hist(m*beta,bins=np.arange(np.amin(m*beta),np.amax(m*beta+binsize),binsize), weights=weights)
 
-#plt.plot(m,wmm)
+print 'max before norm =',np.amax(wmm)
+print np.argmax(wmm)
+wmm /=np.amax(wmm)
+print wmm
+print 'max after norm =', np.amax(wmm)
+
+#plt.plot(m*beta,wmm)
+#plt.plot(centers,counts)
+plt.loglog(m*beta,wmm)
 plt.loglog(centers,counts)
-#plt.semilogy(m,wm)
+plt.ylabel('P(m)')
+plt.xlabel('money $m$')
+plt.tight_layout()
 plt.show()
